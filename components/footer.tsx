833bc33ecbac1efcceade82654accf912675268b
Lilit <baregamyanlilit36@gmail.com>
2026-04-15 01:59:14 +0400

    .

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className=" bottom-0 py-10 text-center text-base bg-[#67443A]   text-[#FFF7EB]">
+    <footer className=" bottom-0 py-10 text-center text-base bg-[#EFCCC8]   text-vrayi">
 
       <Link href="https://www.instagram.com/siteup.am/">
         <p className="FontArmDecorativeU mt-2 text-xl">
